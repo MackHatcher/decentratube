5ddef9605f1ef60cebaa755a72eb1055ef183113
MackHatcher <mack.hatcher1@outlook.com>
2022-10-30 07:10:00 -0500

    misc tweaks

--- truffle-config.js
+++ truffle-config.js
@@ -9,18 +9,18 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "1337" // Match any network id
     },
-    ropsten: {
+    goerli: {
       provider: function() {
         return new HDWalletProvider(
           privateKeys.split(','), // Array of account private keys
-          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
+          `https://eth-goerli.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_API_KEY_GOERLI}`// Url to an Ethereum Node
         )
       },
       gas: 5000000,
       gasPrice: 25000000000,
-      network_id: 3
+      network_id: 5
     }
   },
   contracts_directory: './src/contracts/',
